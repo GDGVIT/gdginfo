2e905423f8645f22cd4769d23d534f8577158455
Rahul Krishna <rkbalu524@gmail.com>
2017-01-24 12:32:57 +0530

    Fixes #1

--- server/main.js
+++ server/main.js
@@ -10,10 +10,12 @@ const axios=require('axios')
 
 const app = express()
 
+require('dotenv').config()
+
 
 // API Calls follow
 app.get('/info',function(req,res){
-    axios.get('https://api.github.com/orgs/GDGVIT?client_id=e63b429174efcee3f453&client_secret=baf28b3b72e252c8d54180bfa0b9706e90caa33c')
+    axios.get('https://api.github.com/orgs/GDGVIT?client_id='+process.env.client_id+'&client_secret='+process.env.client_secret)
     .then(function(response){
       res.send(response.data);
     })
@@ -23,7 +25,7 @@ app.get('/info',function(req,res){
 })
 
 app.get('/users',function(req,res){
-axios.get('https://api.github.com/orgs/GDGVIT/members?client_id=e63b429174efcee3f453&client_secret=baf28b3b72e252c8d54180bfa0b9706e90caa33c')
+axios.get('https://api.github.com/orgs/GDGVIT/membersclient_id='+process.env.client_id+'&client_secret='+process.env.client_secret)
 .then(function(response){
   res.send(response.data);
 }).catch(function(error){
@@ -32,7 +34,7 @@ axios.get('https://api.github.com/orgs/GDGVIT/members?client_id=e63b429174efcee3
 })
 
 app.get('/events',function(req,res){
-axios.get('https://api.github.com/orgs/GDGVIT/events?client_id=e63b429174efcee3f453&client_secret=baf28b3b72e252c8d54180bfa0b9706e90caa33c')
+axios.get('https://api.github.com/orgs/GDGVIT/eventsclient_id='+process.env.client_id+'&client_secret='+process.env.client_secret)
 .then(function(response){
 res.send(response.data);
 })
